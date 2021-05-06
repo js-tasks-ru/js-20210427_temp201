ac95cc166b74ac83b8d08a7e2f5800dd7aaa1273
temp201 <temp201@mail.ru>
2021-05-06 16:31:33 +0300

    Task 3 completed

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === undefined) {
+    return string;
+  }
+  const arr = string.split('');
+  const resArr = [];
+  let counter = 0;
+  let letter = '';
+  arr.forEach(item => {
+    if (item !== letter) {
+      letter = item;
+      counter = 0;
+    }
+    if (counter < size) {
+      resArr.push(letter);
+    }
+    counter++;
+  });
+  return resArr.join('');
 }
